1a3053bdc458cf601ede0d024e47f1543dad25e9
Bloom <bloomtauta@gmail.com>
2022-11-30 21:25:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+// function addingEventListener() {
+//     alert("I eas clicked!");
+// }
+// const input = document.getElementById('button');
+//   function clickAlert() {
+//   alert('I was clicked!');
+// }
+// function clickALert() {
+//   input.addEventListener("click", clickAlert)
+// }
+
+// const input = document.getElementById('input');
+// input.addingEventListener('click', function() {
+//   alert('I was clicked!');
+// });
 function addingEventListener() {
+  const input = document.getElementById('button');
+
+function clickAlert(){
+  alert('I was clicked!');
+}
+input.addEventListener('click', clickAlert)
 }
